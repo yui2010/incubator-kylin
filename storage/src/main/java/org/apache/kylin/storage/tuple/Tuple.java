apache/kylin

KYLIN-666 Select float type column got class cast exception

storage/src/main/java/org/apache/kylin/storage/tuple/Tuple.java
@@ -151,6 +151,8 @@ public class Tuple implements ITuple {
             return new BigDecimal(strValue);
         } else if ("timestamp".equals(dataType)) {
             return Long.valueOf(DateStrDictionary.stringToMillis(strValue));
+        } else if ("float".equals(dataType)){
+            return Float.valueOf(strValue);
         } else {
             return strValue;
         }

yui2010/incubator-kylin

storage/src/main/java/org/apache/kylin/storage/tuple/Tuple.java
@@ -196,6 +196,8 @@ public class Tuple implements ITuple {
             return new BigDecimal(strValue);
         } else if ("timestamp".equals(dataTypeName)) {
             return Long.valueOf(DateFormat.stringToMillis(strValue));
+        } else if ("float".equals(dataTypeName)){
+            return Float.valueOf(strValue);
         } else {
             return strValue;
         }